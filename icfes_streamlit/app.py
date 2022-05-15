d952551c648368a0287bcc04c3db92ddec32ad30
David Felipe Bautista Bernal <davidbautistab@usantotomas.edu.co>
2022-05-15 14:39:35 -0500

    streamlit_icfes_app

--- icfes_streamlit/app.py
+++ icfes_streamlit/app.py
@@ -1,11 +1,15 @@
 import streamlit as st 
-import time
-import streamlit.components.v1 as components
 import seaborn as sns 
 import matplotlib.pyplot as plt 
 import pandas as pd 
+from matplotlib import rcParams
+from processing_data import data_processing_function
 
-df = pd.read_csv('data/data_icfes.csv')
+df = data_processing_function()
+df['Region'].replace({'EXTRANJERO':'Extranjero'},inplace=True)
+
+
+
 
 with st.sidebar:
     st.markdown('''
@@ -16,14 +20,143 @@ with st.sidebar:
      ('Ciencias Naturales', 'Ingles','Lectura Critica','Matemáticas','Sociales Ciudadanas', ''))
 
 
-st.markdown(f'''
+st.markdown(
+'''
 # Análisis Exploratorio Resultados del Examen ICFES Saber 11 - 2019 -II
-#### A continuación, se presentan un reporte de estadísticos descriptivos según la categoría seleccionada en la SideBar de esta App.
+##### Esta APP desarrolla un breve análisis exploratorio sobre los resultados del examen ICFES Saber 11 del 2019-II.\
+ En un principio se realizar una exploración centrada en el comportamiento de las distribuciones del puntaje global\
+del examen asociada con diferentes características categóricas de los individuos. Después de esto se buscará analizar \
+correlaciones entre los puntajes de cada Área 
 
+### **¿Como están caracterizados estos datos?**
+
+##### Antes de visualizar las distribuciones de los puntajes contrastados con algunas categorías de los individuos es pertinente\
+ entender de qué manera dichas categorías conforman la muestra total. A continuación, se utilizarán Tablas y HeatMaps para denotar \
+visualmente la participación de cada grupo especificado dentro del total. 
+
+#### Regiones y estratos de vivienda
 ''')
-st.markdown(f'''
-## Área seleccionada: {area}''')
+
+pivot1 = pd.pivot_table(data = df,index =['Region'], values = 'Puntaje Global' ,columns =['Estrato Vivienda'],aggfunc = 'count')
+st.markdown('''\n'''    
+)
+
+fig1 = plt.figure(figsize=(9,8))
+plt.title('Participación Porcentual de Regiones\n y Estratos de Vivienda', fontsize = 18)
+plt.xticks(rotation = 0)
+pivot1_heat = sns.heatmap((pivot1 *100)/len(df), cmap="viridis",linewidth=1, linecolor='w', square=True, annot = True,
+                          annot_kws={"size":10})
+
+st.pyplot(fig1)
+
+st.markdown('''
+\n
+##### Como la tabla y el grafico anterior lo permite denotar, la Región Andina representa mas del 50% de los registros del total \
+de los datos, y estos en su mayoría focalizados en los estratos Uno, dos y tres. En contraposición, Regiones como la amazónica \
+aportan menos del 2% de los registros. Esto en parte da muestra de la misma naturaleza de las estructuras educativas que están \
+conformadas a lo largo del país, las cuales dependen de la densidad poblacional de cada territorio. Cabe resaltar que lo más \
+probable es que gran parte de los registros que se encuentran dentro de la Región Andina sean de Bogota.DC.   
+
+
+##### De hecho, demos un vistazo rapido a como están conformadas las participaciones por departamento dentro de la Región Andina.
+
+'''
+)
+
+df2 = df[df['Region'] == 'Region Andina']
+pivot2 = pd.pivot_table(data = df2,index =['Departamento'], values = 'Puntaje Global' ,columns =['Estrato Vivienda'],aggfunc = 'count')
+pivot2 = pivot2[pivot2[6]!=0]
+pivot2['Total'] = sum(pivot2[i] for i in range(1,7))
+pivot2.index = [i.capitalize() for i in pivot2.index]
+fig2 = plt.figure(figsize=(9,8))
+plt.title('Participación Porcentual de Departamentos de \nla Region Andina y Estratos de Vivienda', fontsize = 16)
+plt.yticks(fontsize = 12)
+plt.xticks(rotation = 0)
+pivot2_heat = sns.heatmap((pivot2 *100)/len(df2), cmap="viridis",linewidth=1, linecolor='w', square=True, annot = True,
+                          annot_kws={"size":10})
+
+st.pyplot(fig2)
+
+st.markdown('''
+##### Efectivamente, Bogotá acumula el mayor porcentaje de registros entre todos los departamentos de la Región Andina, \
+contando con aproximadamente el 26 % del total de registros de dicha región, por debajo de la capital se encuentran \
+importantes departamentos de Colombia como Antioquia con un 22%.
+
+#### Regiones y Genero 
+
+##### Conociendo ya la participación de los subgrupos de regiones y estratos de vivienda podemos ahora interesarnos por la \
+participación de los géneros dentro del total de los datos. 
+
+##### Para hacer un análisis detallado podemos nuevamente contrastar los subgrupos que se conforman tanto por los géneros como las regiones, \
+aun así, es importante comentar que el total de la muestra analizada está conformado por un 54.2% de Mujeres contra un 45.8% de Hombres. \
+Esta razón entre géneros resulta beneficiosa a la hora de plantear algún tipo de modelo de predicción de puntaje usando estos datos y tomando \
+esta característica como variable de control. 
+''')
+
+
+pivot3 = pd.pivot_table(data = df,index =['Region'], values = 'Puntaje Global' ,columns =['Genero'],aggfunc = 'count')
+
+fig3 = plt.figure(figsize=(7    ,5))
+plt.title('Participación Porcentual de \nGenero por Regiones', fontsize=10)
+plt.xticks(rotation=15, fontsize=8)
+plt.yticks(fontsize=8)
+pivot3_heat = sns.heatmap((pivot3*100)/len(df), cmap="viridis",linewidth=1, linecolor='w', square=True, annot= True,annot_kws={"size":8},)
+# pivot3_heat.set_ylabel('Region',fontsize=8)
+# pivot3_heat.set_xlabel('Genero',fontsize=8)
+st.pyplot(fig3)
+
+st.markdown('''
+##### El grafico anterior permite terminar que en todas las regiones y así mismo en el exterior es mayor el peso porcentual de registros que corresponden \
+a mujeres que los de hombres, sin embargo se mantiene un balance que puede ser importante como lo mencionábamos anteriormente a la hora de usar estos \
+datos en algún modelo de predicción de puntajes. 
+''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if area == 'Ciencias Naturales':
-    fig_naturales = sns.boxplot(data=df,x='punt_c_naturales')
+    fig_naturales= plt.figure(figsize=(8, 6)) 
+    sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})  
+    sns.set_context("notebook", font_scale=1.25)            
+    rcParams['axes.titlepad'] = 20                          
+    fig_naturales.suptitle('Distribución del puntaje por Genero', y=1.01, fontsize=16, fontstyle ='oblique')
+    ax1 = fig_naturales.add_subplot(1,1,1)
+    fig_nat = sns.violinplot(data=df,y='punt_c_naturales', x='estu_genero', ax=ax1)
+    fig_nat.set_xlabel("Genero", fontsize = 16)
+    fig_nat.set_ylabel("Puntaje Ciencias Naturales", fontsize = 16)
+    plt.show()
     st.pyplot(fig_naturales)
+
+    st.markdown('### Genero vs ... ')
+    desp = st.selectbox('',['cole_jornada','cole_caracter'])
+
+    fig_naturales2= plt.figure(figsize=(15, 6)) 
+    sns.set(style='whitegrid', rc={"grid.linewidth": 0.1})  
+    sns.set_context("notebook", font_scale=1.25)            
+    rcParams['axes.titlepad'] = 20                          
+    fig_naturales2.suptitle('Distribución del puntaje por Genero', y=1.01, fontsize=16, fontstyle ='oblique')
+    ax1 = fig_naturales2.add_subplot(1,1,1)
+    fig_nat = sns.violinplot(data=df,y='punt_c_naturales', x=desp,hue='estu_genero', ax=ax1)
+    fig_nat.set_xlabel("Genero", fontsize = 16)
+    fig_nat.set_ylabel("Puntaje Ciencias Naturales", fontsize = 16)
+    st.pyplot(fig_naturales2)
+
+    
--- icfes_streamlit/processing_data.py
+++ icfes_streamlit/processing_data.py
@@ -1,34 +1,43 @@
 from math import floor
-from tkinter.tix import Tree
-from icfes_streamlit.utils import dic_depto
+from utils import dic_depto
 import pandas as pd
 import seaborn as sns 
 
-df = pd.read_csv('data/data_icfes.csv')
-
+def data_processing_function():
+    df = pd.read_csv('data_icfes.csv')
 # Creamos y modificamos algunas variables 
-df['region'] = df['estu_depto_reside'].replace(dic_depto())
+    df['region'] = df['estu_depto_reside'].replace(dic_depto())
 
-df['edad'] = [2020 - int(value.split('/')[2]) for value in df['estu_fechanacimiento']]
-df['edad'].replace({edad:int(df['edad'].mean()) for edad in [2018,2019,5,110,120,2,1,9,6,3,4,7,8,11,10]},inplace=True)
+    df['edad'] = [2020 - int(value.split('/')[2]) for value in df['estu_fechanacimiento']]
+    df['edad'].replace({edad:int(df['edad'].mean()) for edad in [2018,2019,5,110,120,2,1,9,6,3,4,7,8,11,10]},inplace=True)
+
+    df['estu_genero'].replace({1:'Hombre',0:'Mujer'}, inplace=True)
+    df.drop(columns=['estu_fechanacimiento'],inplace=True)
+
+    for col in ['fami_tieneinternet', 'fami_tieneserviciotv', 'fami_tienecomputador', 'fami_tieneautomovil','fami_tieneconsolavideojuegos']:
+        df[col].replace({'0':'No'},inplace=True)
+
+    cols_category = ['estu_genero','estu_etnia','estu_depto_reside','fami_tieneinternet', 'fami_tieneserviciotv',
+        'fami_tienecomputador', 'fami_tieneautomovil','fami_tieneconsolavideojuegos', 'estu_dedicacionlecturadiaria',
+        'estu_dedicacioninternet', 'estu_horassemanatrabaja', 'cole_genero','cole_naturaleza', 'cole_bilingue', 'cole_caracter',
+        'cole_area_ubicacion', 'cole_jornada','region']
+
+    for col in cols_category:
+        df[col] = df[col].astype('category')
+
+    cols_int_8 = ['fami_estratovivienda', 'fami_educacionpadre','fami_educacionmadre','punt_lectura_critica','percentil_lectura_critica', 'punt_matematicas','percentil_matematicas', 'punt_c_naturales', 'percentil_c_naturales',
+    'punt_sociales_ciudadanas', 'percentil_sociales_ciudadanas','punt_ingles', 'percentil_ingles', 'percentil_global','edad']
+
+    for col in cols_int_8:
+        df[col] = df[col].astype('int8')
+   
+    df.columns = ['Genero','Etnia','Departamento','Estrato Vivienda','Educacion Padre','Educacion Madre','Internet','Television','Computadora','Automovil','Consola de Juegos',
+               'Lectura Diaria','Tiempo Internet', 'Horas Trabajo Semanal','Genero Colegio', 'Categoria Colegio', 'Colegio Bilingue','Tipo Colegio','Sector Colegio','Jornada Colegio',
+               'Puntaje Lectura Critica', 'perct_lectura_critica','Puntaje Matematicas', 'percentil_matematicas','Puntaje Ciencias Naturales','percentil_c_naturales',
+               'Puntaje Sociales Ciudadanas','percentil_sociales_ciudadanas','Puntaje Ingles','percentil_ingles','Puntaje Global','percentil_global','Region','Edad' ]
+
+    return df
 
 
-cols_category = ['estu_etnia', 'estu_depto_reside']
-cols_int_8 = ['estu_genero','edad','fami_estratovivienda','fami_educacionpadre','fami_educacionmadre']
-
-for col in cols_category:
-    df[col] = df[col].astype('category')
-
-for col in cols_int_8:
-    df[col] = df[col].astype('int8')
 
 
-df.drop(columns=['estu_fechanacimiento'],inplace=True)
-
-
-df.info()
-
-for i in df.columns:
-    print(i)
-
-sns.boxplot(data=df,x='punt_ingles')
