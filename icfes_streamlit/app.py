152ae54583b6d951f00c15420b228628e517cff0
David Felipe Bautista Bernal <davidbautistab@usantotomas.edu.co>
2022-05-16 01:00:40 -0500

    _test_

--- icfes_streamlit/app.py
+++ icfes_streamlit/app.py
@@ -5,6 +5,39 @@ import pandas as pd
 from matplotlib import rcParams
 # from processing_data import data_processing_function
 
+def dic_depto():
+    dic = {'BOGOTÁ':'Region Andina','ANTIOQUIA':'Region Andina','VALLE':'Region Pacifica',
+    'CUNDINAMARCA':'Region Andina','ATLANTICO':'Region Caribe','SANTANDER':'Region Andina',
+    'BOLIVAR':'Region Caribe','CORDOBA':'Region Caribe','NARIÑO':'Region Pacifica',
+    'BOYACA':'Region Andina', 'MAGDALENA':'Region Caribe', 'TOLIMA':'Region Andina',
+    'NORTE SANTANDER':'Region Andina','HUILA':'Region Andina','CAUCA':'Region Pacifica',
+    'CESAR':'Region Caribe','META':'Region Orinoquia','SUCRE':'Region Caribe',
+    'RISARALDA':'Region Andina','CALDAS':'Region Andina','LA GUAJIRA':'Region Caribe',
+    'QUINDIO':'Region Andina','CASANARE':'Region Orinoquia','CHOCO':'Region Pacifica',
+    'CAQUETA':'Region Amazonica','PUTUMAYO':'Region Amazonica','ARAUCA':'Region Orinoquia',
+    'GUAVIARE':'Region Orinoquia','AMAZONAS':'Region Amazonica','SAN ANDRES':'Region Caribe',
+    'VICHADA':'Region Orinoquia','VAUPES':'Region Amazonica','GUAINIA':'Region Amazonica'}
+    return dic  
+
+
+def dic_educacion():
+    dic = {'Secundaria (Bachillerato) completa': 11,
+            'Primaria incompleta': 3,
+            'Secundaria (Bachillerato) incompleta':8,
+            'Primaria completa':5,
+            'Educación profesional completa':15,
+            'Técnica o tecnológica completa':13,
+            'No sabe':0,
+            '-':0,
+            'Ninguno':0,
+            'Técnica o tecnológica incompleta':12,
+            'Educación profesional incompleta':14,
+            'Postgrado':17,
+            'No Aplica':0}
+    return dic
+
+
+
 def data_processing_function():
     df = pd.read_csv('icfes_streamlit/data_icfes.csv')
 # Creamos y modificamos algunas variables 
